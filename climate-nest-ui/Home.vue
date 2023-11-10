358fad56123de76308a88a93d0c47edac226fce5
Daniel Peters <drpeters@cspire.com>
2023-11-10 16:40:35 -0600

    First few front-end changes (Vue files & main.js)

--- climate-nest-ui/Home.vue
+++ climate-nest-ui/Home.vue
@@ -1,3 +1,20 @@
 <!-- Home.vue -->
 <!-- Purpose: Represents the home page of the application. -->
 <!-- Contents: Vue component for the main landing page. -->
+
+<template>
+    <div>
+      <h1>Welcome to ClimateNest!</h1>
+      <p>This is the home page. Explore the weather and stay prepared!</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Home',
+  };
+  </script>
+  
+  <style scoped>
+  /* Add any styling specific to the Home component */
+  </style>
--- climate-nest-ui/main.js
+++ climate-nest-ui/main.js
@@ -3,7 +3,29 @@
 // Contents: Initializes Vue app and configures plugins.
 
 import Vue from 'vue';
-import { ApolloClient } from 'apollo-client';
+import VueRouter from 'vue-router';
+import Home from './components/Home.vue';
+import App from './App.vue';
+import VueApollo from 'vue-apollo';
+import ApolloClient from 'apollo-boost';
 import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
-import VueApollo from 'vue-apollo';
+
+Vue.config.productionTip = false;
+
+Vue.use(VueApollo);
+
+const apolloClient = new ApolloClient({
+    // You'll add the endponit when you set up your GraphQL server
+    uri: 'YOUR_GRAPHQL_ENDPOINT',
+});
+
+const apolloProvider = new VueApollo({
+    defaultClient: apolloClient,
+});
+
+new Vue({
+    render: h => h(App),
+    apolloProvider,
+
+}).$mount('#app');
